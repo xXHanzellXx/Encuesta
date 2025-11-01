87035a850c2d583254712bd14d43b745d2c6a6bd
xXHanzellXx <165698536+xXHanzellXx@users.noreply.github.com>
2025-11-01 12:52:12 -0600

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-client = MongoClient(os.getenv("MONGO_URI"))
+client = MongoClient(os.getenv("mongodb+srv://hanzell:UwU@cluster0.3wjtgsa.mongodb.net/?appName=Cluster0"))
 db = client["softskills360"]
 users_collection = db["users"]
+
