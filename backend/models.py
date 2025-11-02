47326dc70d9ece2488468bf6fa52bcdc6da41b56
xXHanzellXx <165698536+xXHanzellXx@users.noreply.github.com>
2025-11-02 14:52:29 -0600

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -2,8 +2,12 @@ from pymongo import MongoClient
 import os
 from dotenv import load_dotenv
 
-load_dotenv() 
+load_dotenv() # <--- Esto carga variables de un archivo .env local
+# En Render, las variables de entorno se deben configurar directamente en la plataforma, 
+# no dependen del archivo .env.
 
-client = MongoClient(os.getenv("MONGO_URI")) # <--- Usa la variable
+client = MongoClient(os.getenv("MONGO_URI"))
 db = client["softskills360"]
 users_collection = db["users"]
+all_responses_collection = db["quiz_responses"] # <--- ¡Nueva Colección!
+    
